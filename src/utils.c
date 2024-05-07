8b99682a1d8e18d849dcc4539608c66a0802aaeb
Viktor Czyzewski <viktor.czyzewski@york.ac.uk>
2024-05-07 23:00:54 +0100

    Implemented basic substructure matching (results not parsed)

--- src/utils.c
+++ src/utils.c
@@ -18,18 +18,6 @@ int canon_smiles(char *smiles) {
 }
 
 int substruct_match(char *smiles, char *smarts, char **match) {
-  char *pkl;
-  size_t pkl_size;
-  
-  pkl = get_mol(smiles, &pkl_size, NULL);
-  if (pkl == NULL) { return 1; }
-
-  *match = get_smiles(pkl, pkl_size, NULL);
-  free(pkl);
-
-  return 0;
-  
-  /*
   char *mol_pkl;
   char *qmol_pkl;
   size_t pkl_size;
@@ -44,13 +32,12 @@ int substruct_match(char *smiles, char *smarts, char **match) {
   qmol_pkl = get_qmol(smarts, &pkl_size, NULL);
   if (mol_pkl == NULL) { return 2; }
 
-  match = get_substruct_match(mol_pkl, &pkl_size, qmol_pkl, &pkl_size, NULL);
-  if (match == NULL) { return 3; }
+  *match = get_substruct_match(mol_pkl, pkl_size, qmol_pkl, pkl_size, NULL);
+  if (*match == NULL) { return 3; }
   printf("3");
 
   free(mol_pkl);
   free(qmol_pkl);
 
   return 0;
-  */
 }
