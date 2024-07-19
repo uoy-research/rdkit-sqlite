f5f20697f6ea7177b55a17573505dfb112f9adc7
Viktor Czyzewski <viktor.czyzewski@york.ac.uk>
2024-07-19 14:33:30 +0100

    Removed `mol` function

--- src/main.c
+++ src/main.c
@@ -7,23 +7,6 @@ SQLITE_EXTENSION_INIT1
 
 #include "utils.h"
 
-static void mol_search_func(
-  sqlite3_context *context,
-  int argc,
-  sqlite3_value **argv
-){
-  char *smiles = (char *)sqlite3_value_text(argv[0]);
-
-  int err = canon_smiles(smiles);
-  if (err != 0) {
-    sqlite3_result_error(context, "SMILES parse error", -1);
-    sqlite3_result_null(context);
-    return;
-  }
-
-  sqlite3_result_text(context, smiles, strlen(smiles), SQLITE_TRANSIENT);
-}
-
 static void mol_substruct_func(
   sqlite3_context *context,
   int argc,
@@ -51,10 +34,6 @@ int sqlite3_rdkitsqlite_init(
   int rc = SQLITE_OK;
   SQLITE_EXTENSION_INIT2(pApi);
   enable_logging();
-
-  sqlite3_create_function(db, "mol", 1,
-                   SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
-                   0, mol_search_func, 0, 0);
 
   sqlite3_create_function(db, "substruct_match", 2,
                    SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
